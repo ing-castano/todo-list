d62f1205f789b67f27b8f8beef9c5689f9696cde
Martin Castaño <castanio.m@gmail.com>
2023-09-18 02:33:39 -0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "no-unused-vars": "off", // para que no de error si importamos o declaramos algo y no lo usamos
+    "react/propo-types": "off", // ya lo vamos a ver
+    "no-debugger": "off" // para poder usar el javascript debugger
+  },
+}
